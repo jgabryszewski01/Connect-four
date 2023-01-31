4a197678ce87281dc86f7dbdb5f76353595da002
jgabryszewski01 <jgabryszewski01@gmail.com>
2023-01-31 20:03:41 +0100

    20-03

--- app/src/main/java/com/example/c4/MainActivity.java
+++ app/src/main/java/com/example/c4/MainActivity.java
@@ -3,28 +3,43 @@ package com.example.c4;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayout;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.LinearLayout;
 
 import java.text.BreakIterator;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     int player=0;
     int[] state = {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
-    private Button reset_button;
-    private BreakIterator turnText;
     boolean gameEnded = false;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        float density = displayMetrics.density;
+        int screenWidth = displayMetrics.widthPixels;
+        int screenHeight = displayMetrics.heightPixels;
+        int screenWidthInDp = (int) Math.ceil((screenWidth/density)-0.5f);
+        int screenHeightInDp = (int) Math.ceil((screenHeight/density)-0.5f);
+        if(screenWidthInDp==393 || screenHeightInDp==393){
+            setContentView(R.layout.activity_main393);
+        }
+        else if(screenWidthInDp==731 || screenHeightInDp==731){
+            setContentView(R.layout.activity_main731h);
+        }
+        else{
+            setContentView(R.layout.activity_main);
+        }
     }
 
     public void dropin(View view){
@@ -47,14 +62,26 @@ public class MainActivity extends AppCompatActivity {
                 player = 0;
             }
             counter.animate().translationYBy(1000f).setDuration(400);
-            if(checkforwin()){
-                LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLayout);
-                layout.setVisibility(View.VISIBLE);
-                gameEnded=true;
-                TextView winText = (TextView) findViewById(R.id.winText);
-                if(player==1){winText.setText("Blue has won!");}
-                else{winText.setText("Black has won!");}
+            if(notTie()) {
+                if (checkforwin()) {
+                    LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+                    layout.setVisibility(View.VISIBLE);
+                    gameEnded = true;
+                    TextView winText = (TextView) findViewById(R.id.winText);
+                    if (player == 1) {
+                        winText.setText("Blue has won!");
+                    } else {
+                        winText.setText("Black has won!");
+                    }
+                }
             }
+            else{
+                LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+                layout.setVisibility(View.VISIBLE);
+                gameEnded = true;
+                TextView winText = (TextView) findViewById(R.id.winText);
+                winText.setText("Tie!");
+                }
         }
     }
 
@@ -124,6 +151,12 @@ public class MainActivity extends AppCompatActivity {
         TextView turnText = (TextView) findViewById(R.id.turnText);
         turnText.setText("Current turn: \nBlue");
         gameEnded = false;
+    }
+    public boolean notTie(){
+        for(int i=0;i<42;i++){
+            if(state[i]==2){return true;}
+        }
+        return false;
     }
 }
 
---
+++ app/src/main/java/com/example/c4/StartActivity.java
@@ -0,0 +1,29 @@
+package com.example.c4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+    private Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        startButton = (Button) findViewById(R.id.buttonStart);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMain();
+            }
+        });
+    }
+    public void openMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
